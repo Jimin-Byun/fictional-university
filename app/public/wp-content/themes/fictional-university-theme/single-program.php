c3c8aa69bfd9e03c24079d996ec4349f2ad797c8
Jimin Byun <jimin.stella@gmail.com>
2020-04-09 17:26:15 -0700

    add new professor post type

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -1,5 +1,5 @@
 <?php 
-
+  the_ID();
   get_header(); 
   while(have_posts()) {
     the_post(); ?>
@@ -21,6 +21,38 @@
       <div class="generic-content"><?php the_content(); ?></div>
 
         <?php 
+          $relatedProfessors = new WP_Query(array(
+           'posts_per_page' => 2,
+           'post_type' => 'professor',
+           'orderby' => 'title',
+           'order' => 'ASC',
+           'meta_query' => array(
+             array(
+               'key' => 'related_programs',
+               'compare' => 'LIKE',
+               'value' => '"' . get_the_ID() . '"'
+             )
+           )
+         ));
+
+         if ($relatedProfessors->have_posts()) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+
+          echo '<ul class="professor-cards">';
+          while ($relatedProfessors->have_posts()) {
+            $relatedProfessors->the_post();?>
+            <li class="professor-card__list-item">
+              <a class="professor-card" href="<?php the_permalink() ?>">
+                <img class="prfessor-card__image" src="<?php the_post_thumbnail_url(); ?>">
+                <span class="professor-card__name"><?php the_title(); ?></span>
+              </a>
+            </li>
+          <?php }
+          echo '</ul>';
+         }
+
+          wp_reset_postdata();
           $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
            'posts_per_page' => 2,
@@ -46,7 +78,7 @@
          if ($homepageEvents->have_posts()) {
           echo '<hr class="section-break">';
           echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
-
+          the_ID(); 
           while ($homepageEvents->have_posts()) {
             $homepageEvents->the_post();?>
               <div class="event-summary">
